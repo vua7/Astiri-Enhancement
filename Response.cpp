8a48f58afc4cbc5d6aa61ddd9e1042349d4b220c
Albert <vua7@mymacewan.ca>
2022-05-03 06:23:12 -0600

    Add URI processing response time capturing

--- Response.cpp
+++ Response.cpp
@@ -15,8 +15,8 @@ using namespace std;
 class DerivedRequest: public Request
 {
 private:
-    vector<float> m_uri1;
-    vector<float> m_uri2;
+    vector<std::chrono::milliseconds> m_uri1;
+    vector<std::chrono::milliseconds> m_uri2;
 
 protected:
     /**
@@ -37,15 +37,16 @@ public:
     /**
      *
      */
-    void capture(const std::string& uri) {
+    void capture(const std::string& uri, std::chrono::milliseconds time) {
+        // determine which URI was specified
         if (uri.compare("uri1") == 0) {
-            
+            m_uri1.push_back(time);
         }
         else if (uri.compare("uri2") == 0) {
-            
+            m_uri2.push_back(time);
         }
         else {
-            
+            cerr << "Invalid URI\n";
         }	
     }
 
@@ -75,24 +76,31 @@ public:
  *
  */
 int main(int argc, char *argv[]) {
-    int a, b;
+    int iter1, iter2;
 
+    // object instaniation
     DerivedRequest obj = DerivedRequest();    
     
+    // convert command-line argument to integer
     std::istringstream iss1(argv[2]);
-    iss1 >> a;
-    for (int i = 0; i < a; i++) {
-        obj.capture(argv[1]);
+    iss1 >> iter1;
+    // process first URI
+    for (int i = 0; i < iter1; i++) {
+        auto start = chrono::steady_clock::now();
         obj.process(argv[1]);
-        
+        auto end = chrono::steady_clock::now();
+        obj.capture(argv[1], chrono::duration_cast<chrono::milliseconds>(end - start));
     }
 
+    // convert command-line argument to integer
     std::istringstream iss2(argv[4]);
-    iss2 >> b;
-    for (int j = 0; j < b; j++) {
-        obj.capture(argv[3]);
+    iss2 >> iter2;
+    // process second URI
+    for (int j = 0; j < iter2; j++) {
+        auto start = chrono::steady_clock::now();
         obj.process(argv[3]);
-        
+        auto end = chrono::steady_clock::now();
+        obj.capture(argv[3], chrono::duration_cast<chrono::milliseconds>(end - start));
     }
     
     return 0;
